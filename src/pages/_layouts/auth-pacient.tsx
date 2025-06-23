60f1c8cc766f2adc4695d7861543b3a49e9be582
Gabriel092005 <gabrielcavalamanuel@gmail.com>
2025-06-23 20:34:33 +0100

    dash

--- src/pages/_layouts/auth-pacient.tsx
+++ src/pages/_layouts/auth-pacient.tsx
@@ -7,6 +7,7 @@ export function AuthLayoutPacient(){
     <div className="lg:min-h-screen lg:grid lg:grid-cols-2 antialiased dark:bg-black">
       <div className="lg:justify-between h-full border-r border-foreground/5   lg p-10 text-muted-foreground flex flex-col">
             <div className="flex items-center gap 3 text-lg font-medium text-foreground">
+           
             </div>
             </div>
           <div className="flex flex-col items-center justify-center">
